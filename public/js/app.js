f585888df79faea48436c531cdee8a4814bf817d
robbie <robbielcampbell@hotmail.com>
2020-10-27 14:57:48 +0000

    :sparkles: dark mode now saves in cache :sparkles:

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,41 @@
 const body = document.getElementsByTagName("BODY")[0];
 const darkMode = document.getElementById("dark-mode");
 const nav = document.getElementsByTagName("nav")[0];
-const captionText = document.getElementsByTagName("caption")[0];
+let forms = document.getElementById("form");
+let getDarkModeSetting = window.localStorage.getItem("darkModeValue");
 
-darkMode.addEventListener("click", function(){
-    nav.classList.toggle("bg-dark");
-    nav.classList.toggle("navbar-dark");
-    body.classList.toggle("darker");
-    captionText.toggle("darker");
+// Add dark mode to the page
+function enableDarkMode(){
+    nav.classList.add("bg-dark");
+    nav.classList.add("navbar-dark");
+    body.classList.add("darker");
+    forms.classList.add("bg-dark");
+    window.localStorage.setItem("darkModeValue", "enabled");
+}
+
+// Remove dark mode from the page
+function disableDarkMode(){
+    nav.classList.remove("bg-dark");
+    nav.classList.remove("navbar-dark");
+    body.classList.remove("darker");
+    forms.classList.remove("bg-dark");
+    window.localStorage.setItem("darkModeValue", "disabled");
+}
+
+// Set dark mode from memory
+if (getDarkModeSetting === "enabled")
+{
+    enableDarkMode();
+}
+
+// Set the dark mode setting onclick
+darkMode.addEventListener("click", ()=>{
+    getDarkModeSetting = window.localStorage.getItem("darkModeValue");
+    if (getDarkModeSetting !== "enabled") {
+        enableDarkMode();
+    }
+    else
+    {
+        disableDarkMode();
+    }
 });
